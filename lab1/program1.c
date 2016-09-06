229eff4b1676767faa3b87d896107e6a0cd7fecd
OhYongTaek <ka123ak1@gmail.com>
2016-09-06 17:31:46 +0900

    program1.c add function for print phone

--- lab1/program1.c
+++ lab1/program1.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
+void print(int num);
+void print_email();
+void print_phone();
 int main(int argc, char *argv[]) {
 	int num;
 	scanf("%d",&num);
-	if(num == 1) {
-		printf("ka123ak1@gmail.com\n");
-	}
+	print(num);
 	return 0;
 }
+void print(int num) {
+	if(num == 1) {
+		print_email();	
+	} else if(num == 2) {
+		print_phone();
+	}
+}
+void print_email(){
+	printf("xxx@xxx.com\n");
+}
+void print_phone(){
+	printf("010-0000-0000\n");
+}
+
